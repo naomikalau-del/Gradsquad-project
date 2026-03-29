692bc4fe57f9c0d591a2aebe40105e61e4d2a6ca
Naomikalau-Del <naomikalau@gmail.com>
2026-03-29 01:56:17 +0100

    Add CORS for socket and login connection guard

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ const multer = require('multer');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
 
 // --- Middleware ---
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
